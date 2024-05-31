a64d8580756077c8b415598f1b6cc5cd099aabe2
ChienChihYeh <miroyeh1986@gmail.com>
2024-05-31 11:53:14 +0800

    modified accessibility

--- src/components/PopoverHoverWrapper.vue
+++ src/components/PopoverHoverWrapper.vue
@@ -3,6 +3,7 @@ import { ref, watchEffect } from 'vue'
 import PopoverWrapper from '@/components/PopoverWrapper.vue'
 const popover = ref()
 const isHover = ref(false)
+const popoverId = 'popover'
 interface Props {
     title: string
     description: string
@@ -26,12 +27,10 @@ watchEffect(() => {
         class="popover-hover-trigger w-fit h-fit"
         @mouseenter="isHover = true"
         @mouseleave="isHover = false"
-        tabindex="0"
-        aria-haspopup="true"
-        :aria-expanded="isHover"
+        :aria-describedby="isHover ? popoverId : undefined"
     >
         <slot></slot>
-        <PopoverWrapper ref="popover" v-if="isHover">
+        <PopoverWrapper ref="popover" v-if="isHover" :id="popoverId">
             <div class="bg-slate-700 p-6 rounded-lg my-2 mr-5 max-w-lg">
                 <h3 class="text-gray-50 text-xl mb-3 font-semibold">{{ title }}</h3>
                 <p class="text-gray-50">{{ description }}</p>
